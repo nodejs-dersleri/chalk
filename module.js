9aed8ace453ff539adc45cc52957a301b4782ee2
arifcakiroglu <cakirogluarif@gmail.com>
2015-11-05 22:30:46 +0200

    fixes

--- module.js
+++ module.js
@@ -10,13 +10,13 @@ module.exports= function(text,type){
     bold:       [1, 22],
     underline:  [4, 24],
     black:      [30, 39],
-		red:        [31, 39],
-		green:      [32, 39],
+    red:        [31, 39],
+    green:      [32, 39],
     bgBlack:    [40, 49],
-		bgRed:      [41, 49],
-		bgGreen:    [42, 49],
-		bgYellow:   [43, 49],
-		bgBlue:     [44, 49]
+    bgRed:      [41, 49],
+    bgGreen:    [42, 49],
+    bgYellow:   [43, 49],
+    bgBlue:     [44, 49]
   }
 
   function wrap(args) {
